8d3c71564b1809298c87ea2be0c238c28ffe1eba
Alexis Placet <alexis.placet@quantstack.net>
2025-12-02 15:50:15 +0100

    fix

--- tests/main.cpp
+++ tests/main.cpp
@@ -12,9 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest/doctest.h>
 
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <string>
 
 #include <sparrow_extensions/config/sparrow_extensions_version.hpp>
